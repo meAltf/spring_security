46c627d5fd936cfc27dc096d7476ead3a90700af
meAltf <alataf.dce.cs1@gmail.com>
2025-08-10 20:32:57 +0530

    create custom authentication provider not spring security default

--- src/main/java/com/robert/springSecurityLearn/Configuration/SecurityConfiguration.java
+++ src/main/java/com/robert/springSecurityLearn/Configuration/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.robert.springSecurityLearn.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,12 +14,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -39,6 +46,7 @@ public class SecurityConfiguration {
                 .build();
     }
 
+/**
     @Bean
     public UserDetailsService userDetailsService() {
 
@@ -57,6 +65,15 @@ public class SecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+    **/
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setUserDetailsService(userDetailsService);
+        return authProvider;
     }
 }
 
