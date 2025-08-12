0266df224bd4c383c8ed63126cc2ec075d1048b7
meAltf <alataf.dce.cs1@gmail.com>
2025-08-12 23:20:03 +0530

    register user | with Bcrypt encoder

--- src/main/java/com/robert/springSecurityLearn/Controller/UserController.java
+++ src/main/java/com/robert/springSecurityLearn/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.robert.springSecurityLearn.Service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,12 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(13);
+
     @PostMapping("/register")
     public ResponseEntity<Users> registerUser(@RequestBody Users newUser) {
+
+        newUser.setPassword(encoder.encode(newUser.getPassword()));
         usersService.registerUser(newUser);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
