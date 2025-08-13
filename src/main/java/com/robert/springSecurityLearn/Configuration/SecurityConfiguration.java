b2a2792bab8422b3e668e724ff9770873a3cd8ed
meAltf <alataf.dce.cs1@gmail.com>
2025-08-13 23:22:27 +0530

    JWT dependency & get AuthenticationManager

--- src/main/java/com/robert/springSecurityLearn/Configuration/SecurityConfiguration.java
+++ src/main/java/com/robert/springSecurityLearn/Configuration/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package com.robert.springSecurityLearn.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -76,6 +78,11 @@ public class SecurityConfiguration {
         authProvider.setPasswordEncoder(new BCryptPasswordEncoder(13));
         authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
 
